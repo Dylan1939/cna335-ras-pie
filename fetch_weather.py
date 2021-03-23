728f5c0b7874094e879c5e62f6bfefafe165e022
Dylan1939 <61760435+Dylan1939@users.noreply.github.com>
2021-03-22 19:09:28 -0700

    Delete fetch_weather.py

--- fetch_weather.py
+++
@@ -1,7 +0,0 @@
-from pprint import pprint
-
-from requests import get
-
-url = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getlatestwsdata/1648902'
-weather = get(url).json()['items']
-pprint(weather)
